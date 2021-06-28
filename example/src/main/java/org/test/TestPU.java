bdd47195406572efa9c34fb5273aa0e92402c7a1
Andrew <65760867+VolanDeMor1@users.noreply.github.com>
2021-06-28 12:18:35 +0300

    Add files via upload

---
+++ example/src/main/java/org/test/TestPU.java
@@ -0,0 +1,32 @@
+package org.test;
+
+import fun.asgard.PaperUpdater;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+
+public final class TestPU extends JavaPlugin {
+
+    public static PaperUpdater PU;
+
+    @Override
+    public void onEnable() {
+        PU = new PaperUpdater(this, getServer().getMinecraftVersion())
+                .setJVMOptions("-Xmx7G -Xms7G")
+                .setStartingFile("start.sh");
+
+        PU.removeUnneededPapers().onComplete(complete -> System.out.println(Arrays.toString(complete.getFiles())));
+
+        if (PU.isAvailableUpdate()) {
+            System.out.println("Есть новые обновы (" + PU.getCurrentPaperBuild() + "/" + PU.getLatestBuild() + ")");
+            System.out.println("Скачивание новой версии началось...");
+            PU.downloadLatestPaper().onComplete(downloadAction -> System.out.println("Скачивание завершено! Перезапустите сервер, чтобы применились обновления"));
+        } else {
+            System.out.println("Нет обнов");
+        }
+    }
+
+    @Override
+    public void onDisable() { }
+
+}
